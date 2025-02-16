dff893f3d5f49a09165a3c99440b157ac2ee3140
SurKaa <98200894+surkaa@users.noreply.github.com>
2025-02-17 00:55:41 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="main">
+    <h1>Fun Web</h1>
+  </div>
+</template>
+
+<style scoped>
+#main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
